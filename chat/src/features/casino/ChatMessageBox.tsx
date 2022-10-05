8d0f4a282e09c679a195785d879e6368291c57ce
Outrun Colors <itsoutruncolors@gmail.com>
2022-10-04 23:15:40 -0500

    Correct message batching logic

--- chat/src/features/casino/ChatMessageBox.tsx
+++ chat/src/features/casino/ChatMessageBox.tsx
@@ -1,112 +1,131 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
 import { Avatar, Comment, Dropdown, Menu, Popconfirm, Typography } from "antd";
 import type { ItemType } from "antd/lib/menu/hooks/useItems";
+import key from "weak-key";
 import { formatTimeAgo } from "../../helpers";
 import { useRootContext } from "../../hooks";
 import { useCasino } from "./useCasino";
-import { useCasinoSelector } from "./state";
+import { useChatMessageGroups } from "./state";
 
 const { Text } = Typography;
 
 export function ChatMessageBox() {
-  const { id, admin } = useRootContext();
-  const { recipient, setRecipient, userDeletedMessage } = useCasino();
-  const chatMessages = useCasinoSelector((state) => {
-    let messages = state.message.all.map(
-      (messageId) => state.message.by_id[messageId]
-    );
-
-    if (recipient) {
-      const conversationKey = state.conversation.all.find(
-        (key) => key.includes(id) && key.includes(recipient)
-      );
-
-      if (conversationKey) {
-        const conversationMessages =
-          state.conversation.by_id[conversationKey].messages;
-
-        messages = conversationMessages.all.map(
-          (messageId) => conversationMessages.by_id[messageId]
-        );
-      }
-    }
-
-    return messages.map((message) => {
-      return {
-        message,
-        author: state.user.by_id[message.user_id],
-      };
-    });
-  });
+  const { recipient, setRecipient } = useCasino();
+  const chatMessageGroups = useChatMessageGroups();
 
   return (
     <>
       {recipient && <h3 onClick={() => setRecipient("")}>{recipient}</h3>}
-      {chatMessages.map((chatMessage) => {
-        const items: ItemType[] = [
-          {
-            key: "react",
-            label: "React",
-            disabled: true,
-          },
-        ];
-        const isOwnMessage = id.toString() === chatMessage.author.id.toString();
 
-        if (!isOwnMessage) {
-          items.unshift(
-            {
-              key: "reply",
-              label: "Reply",
-              disabled: true,
-            },
-            {
-              key: "dm",
-              label: "DM",
-              disabled: true,
-            }
-          );
-        }
-
-        if (isOwnMessage || admin) {
-          items.push(
-            { key: "edit", label: "Edit", disabled: true },
-            {
-              key: "delete",
-              label: (
-                <Popconfirm
-                  title="Delete this message?"
-                  onConfirm={() => userDeletedMessage(chatMessage.message.id)}
-                  okText="Yes"
-                  cancelText="No"
-                >
-                  <Text type="danger">Delete</Text>
-                </Popconfirm>
-              ),
-            }
-          );
-        }
-
-        const menu = <Menu items={items} />;
-
-        return (
-          <div key={chatMessage.message.id}>
-            <Dropdown overlay={menu} trigger={["click"]}>
-              <Comment
-                key={chatMessage.message.id}
-                avatar={
-                  <Avatar
-                    src={chatMessage.author.account.profile_url}
-                    alt={chatMessage.author.account.username}
-                  />
-                }
-                author={chatMessage.author.account.username}
-                datetime={formatTimeAgo(chatMessage.message.timestamp)}
-                content={chatMessage.message.text}
-              />
-            </Dropdown>
-          </div>
-        );
-      })}
+      {chatMessageGroups.map((chatMessageGroup) => (
+        <ChatMessageGroup
+          key={key(chatMessageGroup)}
+          author={chatMessageGroup.author}
+          messages={chatMessageGroup.messages}
+        />
+      ))}
     </>
   );
+}
+
+interface ChatMessageGroupProps extends PropsWithChildren {
+  author: UserEntity;
+  messages: MessageEntity[];
+}
+
+export function ChatMessageGroup({ author, messages }: ChatMessageGroupProps) {
+  const [first, ...rest] = messages;
+
+  return (
+    <Comment
+      avatar={
+        <Avatar
+          src={author.account.profile_url}
+          alt={author.account.username}
+        />
+      }
+      author={author.account.username}
+      datetime={formatTimeAgo(first.timestamp)}
+      content={
+        <div style={{ position: "relative" }}>
+          {first.text}
+
+          <Dropdown.Button
+            style={{ position: "absolute", top: 0, right: 0 }}
+            overlay={<ChatMessageMenu author={author} message={first} />}
+          />
+        </div>
+      }
+    >
+      {rest.map((message) => (
+        <Comment
+          key={key(message)}
+          content={
+            <div style={{ position: "relative" }}>
+              {message.text}
+
+              <Dropdown.Button
+                style={{ position: "absolute", top: 0, right: 0 }}
+                overlay={<ChatMessageMenu author={author} message={message} />}
+              />
+            </div>
+          }
+        />
+      ))}
+    </Comment>
+  );
+}
+
+interface ChatMessageMenuProps {
+  author: UserEntity;
+  message: MessageEntity;
+}
+
+export function ChatMessageMenu({ author, message }: ChatMessageMenuProps) {
+  const { id, admin } = useRootContext();
+  const { userDeletedMessage } = useCasino();
+  const items: ItemType[] = [
+    {
+      key: "react",
+      label: "React",
+      disabled: true,
+    },
+  ];
+  const isOwnMessage = id.toString() === author.id.toString();
+
+  if (!isOwnMessage) {
+    items.unshift(
+      {
+        key: "reply",
+        label: "Reply",
+        disabled: true,
+      },
+      {
+        key: "dm",
+        label: "DM",
+        disabled: true,
+      }
+    );
+  }
+
+  if (isOwnMessage || admin) {
+    items.push(
+      { key: "edit", label: "Edit", disabled: true },
+      {
+        key: "delete",
+        label: (
+          <Popconfirm
+            title="Delete this message?"
+            onConfirm={() => userDeletedMessage(message.id)}
+            okText="Yes"
+            cancelText="No"
+          >
+            <Text type="danger">Delete</Text>
+          </Popconfirm>
+        ),
+      }
+    );
+  }
+
+  return <Menu items={items} />;
 }
--- chat/src/features/casino/state/hooks.ts
+++ chat/src/features/casino/state/hooks.ts
@@ -1,6 +1,49 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from './store';
+import cloneDeep from "lodash.clonedeep";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "./store";
 
-export const useCasinoDispatch: () => AppDispatch = useDispatch
+export const useCasinoDispatch: () => AppDispatch = useDispatch;
 
-export const useCasinoSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useCasinoSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export function useCasinoUserLookup() {
+  return useCasinoSelector((state) => state.user.by_id);
+}
+
+export function useChatMessageGroups() {
+  const userLookup = useCasinoUserLookup();
+  const messages = useCasinoSelector((state) =>
+    state.message.all
+      .map((messageId) => state.message.by_id[messageId])
+      .sort((messageA, messageB) => messageA.timestamp - messageB.timestamp)
+  );
+  const chatMessageGroups = [] as Array<{
+    author: UserEntity;
+    messages: MessageEntity[];
+  }>;
+
+  if (messages.length === 0) {
+    return chatMessageGroups;
+  } else {
+    const chatMessageGroup = {
+      author: userLookup[messages[0].user_id],
+      messages: [] as MessageEntity[],
+    };
+
+    for (const message of messages) {
+      if (message.user_id === chatMessageGroup.author.id) {
+        // Same user: group in with previous messages.
+        chatMessageGroup.messages.push(message);
+      } else {
+        // New user: Finish off the previous batch and start a new one.
+        chatMessageGroups.push(cloneDeep(chatMessageGroup));
+        chatMessageGroup.author = userLookup[message.user_id];
+        chatMessageGroup.messages = [message];
+      }
+    }
+
+    chatMessageGroups.push(cloneDeep(chatMessageGroup));
+
+    return chatMessageGroups;
+  }
+}
