d3f5606d6e665a02c0373b1bac28dbe344d440b4
justcool393 <justcool393@gmail.com>
2022-11-09 05:57:37 -0600

    hdjbjdhbhjf

--- files/routes/__init__.py
+++ files/routes/__init__.py
@@ -3,7 +3,7 @@ from files.helpers.const import FEATURES
 
 # import flask then...
 from flask.wrappers import Response
-from flask import g, request, render_template, make_response
+from flask import * # g, request, render_template, make_response, redirect, jsonify, send_from_directory
 
 # import our app then...
 from files.__main__ import app
