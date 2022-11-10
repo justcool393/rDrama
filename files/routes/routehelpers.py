af545475cb53eb5b6ed86ad1cc2a26ff4d557460
justcool393 <justcool393@gmail.com>
2022-11-10 01:18:15 -0600

    route helper

--- files/routes/routehelpers.py
+++ files/routes/routehelpers.py
@@ -9,7 +9,8 @@ from files.helpers.security import generate_hash, validate_hash
 def get_raw_formkey(u:User):
 	return f"{session['session_id']}+{u.id}+{u.login_nonce}"
 
-def get_formkey(u:User):
+def get_formkey(u:Optional[User]):
+	if not u: return "" # if no user exists, give them a blank formkey
 	return generate_hash(get_raw_formkey(u))
 
 def validate_formkey(u:User, formkey:Optional[str]) -> bool:
