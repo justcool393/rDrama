09a6fa96ebb1ea90ab610005de7e8d9371270bd9
justcool393 <justcool393@gmail.com>
2022-11-09 20:27:48 -0600

    print the entire session

--- files/__main__.py
+++ files/__main__.py
@@ -103,6 +103,7 @@ def before_request():
 	if not request.path: request.path = '/'
 	request.full_path = request.full_path.rstrip('?').rstrip('/')
 	if not request.full_path: request.full_path = '/'
+	print(session) ###### REMOVE BEFORE FLIGHT ######
 	if not session.get("session_id"):
 		session.permanent = True
 		session["session_id"] = secrets.token_hex(49)
