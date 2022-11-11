e37b359112be92de2aaba2e96b6c6f1101a7c9b1
justcool393 <justcool393@gmail.com>
2022-11-10 18:33:49 -0600

    testing allroutes

--- files/__main__.py
+++ files/__main__.py
@@ -3,7 +3,6 @@ import gevent.monkey
 gevent.monkey.patch_all()
 
 import faulthandler
-import secrets
 from os import environ
 from sys import argv, stdout
 
@@ -71,57 +70,7 @@ const_initialize(db_session)
 cache = Cache(app)
 Compress(app)
 
-@app.before_request
-def before_request():
-	app.config["SETTINGS"] = CONFIG
-	if SITE == 'marsey.world' and request.path != '/kofi':
-		abort(404)
-
-	g.agent = request.headers.get("User-Agent")
-	if not g.agent and request.path != '/kofi':
-		return 'Please use a "User-Agent" header!', 403
-
-	ua = g.agent or ''
-	ua = ua.lower()
-
-	if request.host != SITE:
-		return {"error": "Unauthorized host provided"}, 403
-
-	if request.headers.get("CF-Worker"): return {"error": "Cloudflare workers are not allowed to access this website."}, 403
-
-	if not CONFIG['Bots'] and request.headers.get("Authorization"): abort(403)
-
-	g.db = db_session()
-	g.webview = '; wv) ' in ua
-	g.inferior_browser = 'iphone' in ua or 'ipad' in ua or 'ipod' in ua or 'mac os' in ua or ' firefox/' in ua
-	g.is_tor = request.headers.get("cf-ipcountry") == "T1"
-
-	request.path = request.path.rstrip('/')
-	if not request.path: request.path = '/'
-	request.full_path = request.full_path.rstrip('?').rstrip('/')
-	if not request.full_path: request.full_path = '/'
-	if not session.get("session_id"):
-		session.permanent = True
-		session["session_id"] = secrets.token_hex(49)
-
-@app.after_request
-def after_request(response):
-	if response.status_code < 400:
-		if CLOUDFLARE_AVAILABLE and CLOUDFLARE_COOKIE_VALUE and getattr(g, 'desires_auth', False):
-			logged_in = bool(getattr(g, 'v', None))
-			response.set_cookie("lo", CLOUDFLARE_COOKIE_VALUE if logged_in else '', max_age=60*60*24*365 if logged_in else 1)
-		g.db.commit()
-		g.db.close()
-		del g.db
-	return response
-
-@app.teardown_appcontext
-def teardown_request(error):
-	if getattr(g, 'db', None):
-		g.db.rollback()
-		g.db.close()
-		del g.db
-	stdout.flush()
+from files.routes.allroutes import *
 
 if "load_chat" in argv:
 	from files.routes.chat import *
