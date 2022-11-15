0c386272a7956aa38861edc95f3933d4ee177be3
justcool393 <justcool393@gmail.com>
2022-11-15 00:04:54 -0600

    errors: fix missing import

--- files/routes/errors.py
+++ files/routes/errors.py
@@ -1,7 +1,7 @@
 import time
 from urllib.parse import quote, urlencode
 
-from flask import redirect, render_template, request, session
+from flask import redirect, render_template, request, session, g
 
 from files.helpers.const import ERROR_MARSEYS, ERROR_MSGS, ERROR_TITLES, WERKZEUG_ERROR_DESCRIPTIONS, is_site_url
 from files.__main__ import app
