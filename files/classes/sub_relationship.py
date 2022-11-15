0be5aad06e642c777573df8ca88bbd158365ff5a
justcool393 <justcool393@gmail.com>
2022-11-15 08:46:40 -0600

    fix

--- files/classes/sub_relationship.py
+++ files/classes/sub_relationship.py
@@ -1,6 +1,7 @@
 import time
 
 from sqlalchemy import Column, ForeignKey
+from sqlalchemy.orm import declared_attr
 from sqlalchemy.sql.sqltypes import *
 
 from files.classes import Base
@@ -8,9 +9,18 @@ from files.classes import Base
 class SubRelationship(Base):
 	__tablename__ = NotImplemented
 	__abstract__ = True
-	user_id = Column(Integer, ForeignKey("users.id"), primary_key=True)
-	sub = Column(String(20), ForeignKey("subs.name"), primary_key=True)
-	created_utc = Column(Integer)
+
+	@declared_attr
+	def user_id(self):
+		return Column(Integer, ForeignKey("users.id"), primary_key=True)
+	
+	@declared_attr
+	def sub(self):
+		return Column(String(20), ForeignKey("subs.name"), primary_key=True)
+
+	@declared_attr
+	def created_utc(self):
+		return Column(Integer)
 
 	def __init__(self, *args, **kwargs):
 		if "created_utc" not in kwargs: kwargs["created_utc"] = int(time.time())
