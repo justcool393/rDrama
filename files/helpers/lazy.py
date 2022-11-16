13690a28b532244c52d15d1693a261ccc743d0aa
justcool393 <justcool393@gmail.com>
2022-11-16 04:10:08 -0600

    merge

--- files/helpers/lazy.py
+++ files/helpers/lazy.py
@@ -6,12 +6,9 @@ def lazy(f):
 		o = args[0]
 		if "_lazy" not in o.__dict__:
 			o.__dict__["_lazy"] = {}
-
 		name = f.__name__ + str(args) + str(kwargs),
-
 		if name not in o.__dict__["_lazy"]:
 			o.__dict__["_lazy"][name] = f(*args, **kwargs)
 		return o.__dict__["_lazy"][name]
-
 	wrapper.__name__ = f.__name__
 	return 
